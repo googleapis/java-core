e635406c4189cd2beeae7c7b8fc4118beb47a666
Cody Oss <codyoss@google.com>
2019-12-13 16:19:56 -0700

    use the correct setter

--- google-cloud-core/src/test/java/com/google/cloud/ServiceOptionsTest.java
+++ google-cloud-core/src/test/java/com/google/cloud/ServiceOptionsTest.java
@@ -320,7 +320,7 @@ public class ServiceOptionsTest {
     TestServiceOptions noCredsWithQuotaProject =
         TestServiceOptions.newBuilder()
             .setCredentials(NoCredentials.getInstance())
-            .setQuotaProjectId("project-id")
+            .setProjectId("project-id")
             .setQuotaProjectId("quota-project-id")
             .build();
     TestServiceOptions quotaProjectCredsWithQuotaProject =
@@ -331,7 +331,10 @@ public class ServiceOptionsTest {
     TestServiceOptions quotaProjectCreds =
         TestServiceOptions.newBuilder().setCredentials(credentialsWithQuotaProject).build();
     TestServiceOptions none =
-        TestServiceOptions.newBuilder().setCredentials(NoCredentials.getInstance()).build();
+        TestServiceOptions.newBuilder()
+            .setCredentials(NoCredentials.getInstance())
+            .setProjectId("project-id")
+            .build();
 
     assertEquals("quota-project-id", noCredsWithQuotaProject.getQuotaProjectId());
     assertEquals("quota-project-id", quotaProjectCredsWithQuotaProject.getQuotaProjectId());
