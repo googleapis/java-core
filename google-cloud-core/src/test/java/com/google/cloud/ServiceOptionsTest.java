7720f55fdb928b6982ec5e02000502f4019a21c3
Cody Oss <codyoss@google.com>
2019-12-13 15:52:11 -0700

    add test for order of setting quota project

--- google-cloud-core/src/test/java/com/google/cloud/ServiceOptionsTest.java
+++ google-cloud-core/src/test/java/com/google/cloud/ServiceOptionsTest.java
@@ -53,6 +53,10 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class ServiceOptionsTest {
+  private static GoogleCredentials credentials;
+  private static GoogleCredentials credentialsWithProjectId;
+  private static GoogleCredentials credentialsWithQuotaProject;
+
   private static final String JSON_KEY =
       "{\n"
           + "  \"private_key_id\": \"somekeyid\",\n"
@@ -80,18 +84,37 @@ public class ServiceOptionsTest {
           + "  \"client_id\": \"someclientid.apps.googleusercontent.com\",\n"
           + "  \"type\": \"service_account\"\n"
           + "}";
-  private static GoogleCredentials credentials;
-
-  static {
-    try {
-      InputStream keyStream = new ByteArrayInputStream(JSON_KEY.getBytes());
-      credentials = GoogleCredentials.fromStream(keyStream);
-    } catch (IOException e) {
-      fail("Couldn't create fake JSON credentials.");
-    }
-  }
 
   private static final String JSON_KEY_PROJECT_ID =
+      "{\n"
+          + "  \"private_key_id\": \"somekeyid\",\n"
+          + "  \"private_key\": \"-----BEGIN PRIVATE KEY-----\\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggS"
+          + "kAgEAAoIBAQC+K2hSuFpAdrJI\\nnCgcDz2M7t7bjdlsadsasad+fvRSW6TjNQZ3p5LLQY1kSZRqBqylRkzteMOyHg"
+          + "aR\\n0Pmxh3ILCND5men43j3h4eDbrhQBuxfEMalkG92sL+PNQSETY2tnvXryOvmBRwa/\\nQP/9dJfIkIDJ9Fw9N4"
+          + "Bhhhp6mCcRpdQjV38H7JsyJ7lih/oNjECgYAt\\nknddadwkwewcVxHFhcZJO+XWf6ofLUXpRwiTZakGMn8EE1uVa2"
+          + "LgczOjwWHGi99MFjxSer5m9\\n1tCa3/KEGKiS/YL71JvjwX3mb+cewlkcmweBKZHM2JPTk0ZednFSpVZMtycjkbLa"
+          + "\\ndYOS8V85AgMBewECggEBAKksaldajfDZDV6nGqbFjMiizAKJolr/M3OQw16K6o3/\\n0S31xIe3sSlgW0+UbYlF"
+          + "4U8KifhManD1apVSC3csafaspP4RZUHFhtBywLO9pR5c\\nr6S5aLp+gPWFyIp1pfXbWGvc5VY/v9x7ya1VEa6rXvL"
+          + "sKupSeWAW4tMj3eo/64ge\\nsdaceaLYw52KeBYiT6+vpsnYrEkAHO1fF/LavbLLOFJmFTMxmsNaG0tuiJHgjshB\\"
+          + "n82DpMCbXG9YcCgI/DbzuIjsdj2JC1cascSP//3PmefWysucBQe7Jryb6NQtASmnv\\nCdDw/0jmZTEjpe4S1lxfHp"
+          + "lAhHFtdgYTvyYtaLZiVVkCgYEA8eVpof2rceecw/I6\\n5ng1q3Hl2usdWV/4mZMvR0fOemacLLfocX6IYxT1zA1FF"
+          + "JlbXSRsJMf/Qq39mOR2\\nSpW+hr4jCoHeRVYLgsbggtrevGmILAlNoqCMpGZ6vDmJpq6ECV9olliDvpPgWOP+\\nm"
+          + "YPDreFBGxWvQrADNbRt2dmGsrsCgYEAyUHqB2wvJHFqdmeBsaacewzV8x9WgmeX\\ngUIi9REwXlGDW0Mz50dxpxcK"
+          + "CAYn65+7TCnY5O/jmL0VRxU1J2mSWyWTo1C+17L0\\n3fUqjxL1pkefwecxwecvC+gFFYdJ4CQ/MHHXU81Lwl1iWdF"
+          + "Cd2UoGddYaOF+KNeM\\nHC7cmqra+JsCgYEAlUNywzq8nUg7282E+uICfCB0LfwejuymR93CtsFgb7cRd6ak\\nECR"
+          + "8FGfCpH8ruWJINllbQfcHVCX47ndLZwqv3oVFKh6pAS/vVI4dpOepP8++7y1u\\ncoOvtreXCX6XqfrWDtKIvv0vjl"
+          + "HBhhhp6mCcRpdQjV38H7JsyJ7lih/oNjECgYAt\\nkndj5uNl5SiuVxHFhcZJO+XWf6ofLUregtevZakGMn8EE1uVa"
+          + "2AY7eafmoU/nZPT\\n00YB0TBATdCbn/nBSuKDESkhSg9s2GEKQZG5hBmL5uCMfo09z3SfxZIhJdlerreP\\nJ7gSi"
+          + "dI12N+EZxYd4xIJh/HFDgp7RRO87f+WJkofMQKBgGTnClK1VMaCRbJZPriw\\nEfeFCoOX75MxKwXs6xgrw4W//AYG"
+          + "GUjDt83lD6AZP6tws7gJ2IwY/qP7+lyhjEqN\\nHtfPZRGFkGZsdaksdlaksd323423d+15/UvrlRSFPNj1tWQmNKk"
+          + "XyRDW4IG1Oa2p\\nrALStNBx5Y9t0/LQnFI4w3aG\\n-----END PRIVATE KEY-----\\n\",\n"
+          + "  \"project_id\": \"someprojectid\",\n"
+          + "  \"client_email\": \"someclientid@developer.gserviceaccount.com\",\n"
+          + "  \"client_id\": \"someclientid.apps.googleusercontent.com\",\n"
+          + "  \"type\": \"service_account\"\n"
+          + "}";
+
+  private static final String JSON_KEY_QUOTA_PROJECT_ID =
       "{\n"
           + "  \"private_key_id\": \"somekeyid\",\n"
           + "  \"private_key\": \"-----BEGIN PRIVATE KEY-----\\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggS"
@@ -120,15 +143,21 @@ public class ServiceOptionsTest {
           + "  \"type\": \"service_account\",\n"
           + "  \"quota_project_id\": \"some-quota-project-id\"\n"
           + "}";
-  private static GoogleCredentials credentialsWithProjectId;
 
   static {
+    credentials = loadCredentials(JSON_KEY);
+    credentialsWithProjectId = loadCredentials(JSON_KEY_PROJECT_ID);
+    credentialsWithQuotaProject = loadCredentials(JSON_KEY_QUOTA_PROJECT_ID);
+  }
+
+  static GoogleCredentials loadCredentials(String credentialFile) {
     try {
-      InputStream keyStream = new ByteArrayInputStream(JSON_KEY_PROJECT_ID.getBytes());
-      credentialsWithProjectId = GoogleCredentials.fromStream(keyStream);
+      InputStream keyStream = new ByteArrayInputStream(credentialFile.getBytes());
+      return GoogleCredentials.fromStream(keyStream);
     } catch (IOException e) {
       fail("Couldn't create fake JSON credentials.");
     }
+    return null;
   }
 
   private static final ApiClock TEST_CLOCK = new TestClock();
@@ -148,6 +177,7 @@ public class ServiceOptionsTest {
           .setHost("host")
           .setProjectId("project-id")
           .setRetrySettings(ServiceOptions.getNoRetrySettings())
+          .setQuotaProjectId("quota-project-id")
           .build();
   private static final TestServiceOptions DEFAULT_OPTIONS =
       TestServiceOptions.newBuilder().setProjectId("project-id").build();
@@ -283,7 +313,29 @@ public class ServiceOptionsTest {
     assertSame(CurrentMillisClock.getDefaultClock(), DEFAULT_OPTIONS.getClock());
     assertEquals("https://www.googleapis.com", DEFAULT_OPTIONS.getHost());
     assertSame(ServiceOptions.getDefaultRetrySettings(), DEFAULT_OPTIONS.getRetrySettings());
-    assertEquals("quota-project-id", OPTIONS.getQuotaProjectId());
+  }
+
+  @Test
+  public void testBuilder_quotaProjectServiceOptionTakesPrecedence() {
+    TestServiceOptions noCredsWithQuotaProject =
+        TestServiceOptions.newBuilder()
+            .setCredentials(NoCredentials.getInstance())
+            .setQuotaProjectId("quota-project-id")
+            .build();
+    TestServiceOptions quotaProjectCredsWithQuotaProject =
+        TestServiceOptions.newBuilder()
+            .setCredentials(credentialsWithQuotaProject)
+            .setQuotaProjectId("quota-project-id")
+            .build();
+    TestServiceOptions quotaProjectCreds =
+        TestServiceOptions.newBuilder().setCredentials(credentialsWithQuotaProject).build();
+    TestServiceOptions none =
+        TestServiceOptions.newBuilder().setCredentials(NoCredentials.getInstance()).build();
+
+    assertEquals("quota-project-id", noCredsWithQuotaProject.getQuotaProjectId());
+    assertEquals("quota-project-id", quotaProjectCredsWithQuotaProject.getQuotaProjectId());
+    assertEquals("some-quota-project-id", quotaProjectCreds.getQuotaProjectId());
+    assertEquals(null, none.getQuotaProjectId());
   }
 
   @Test
